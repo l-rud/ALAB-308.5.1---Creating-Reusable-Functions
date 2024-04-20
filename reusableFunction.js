a87b83f27edccdb86b84bf6a7e037ed5b06bc0d7
Larysa Rud <larpavrud@gmail.com>
2024-04-20 12:37:46 -0400

    Take an array of strings and return the longest string.

--- reusableFunction.js
+++ reusableFunction.js
@@ -13,13 +13,29 @@ console.log(sum([10, 2, 3]));
 
 //Take an array of numbers and return the average
 
-const avg = function(avgArray) {
+const avg = function (avgArray) {
     let avgAdd = 0;
     for (let i = 0; i < avgArray.length; i++) {
         let avgAddIndex = avgArray[i];
         avgAdd += avgAddIndex;
-        avgResult = avgAdd/avgArray.length;
+        avgResult = avgAdd / avgArray.length;
     }
     return avgResult;
+}
+console.log(avg([10, 6]));
+
+//Take an array of strings and return the longest string.
+
+function longestString(array) {
+    
+    var longestWord = "";
+  
+    array.forEach(function(word) {
+      if(word.length > longestString.length) {
+        longestString = word;
+      }
+    });
+  
+    return longestString;
   }
- console.log(avg([10, 6]));
+  console.log(longestString(['first string', 'second string is longer', 'third string is the longeeeeeeeeeeeest']));
